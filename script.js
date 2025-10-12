2282690386b9e499b60527244cf8bf48f3162800
swartberg <141254819+swartberg@users.noreply.github.com>
2025-10-12 12:30:16 +0300

    fixed active navitagtion item look

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ navItems.forEach(item => {
         e.preventDefault();
 
         sections.forEach(section => section.classList.remove("img-active"));
+        navItems.forEach(nav => nav.classList.remove("nav-active"));
+
+        item.classList.add("nav-active");
 
         const targetId = item.getAttribute("data-target");
         const targetSection = document.getElementById(targetId);
